4481ddaf2dcfe11914ff9c1ae1c23f0f987899f6
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-01-24 14:26:53 -0300

    Finaling modal structure and simples functions

--- src/script.js
+++ src/script.js
@@ -1,11 +1,7 @@
 const modal = {
     element: document.querySelector('.modal-overlay'),
-    open: () => {
-        modal.element.classList.add('active')
-    },
-    close: () => {
-        modal.element.classList.remove('active')
-    },
+    open: () => modal.element.classList.add('active'),
+    close: () => modal.element.classList.remove('active'),
 }
 
 document.querySelector('.button.new').addEventListener('click', () => {
