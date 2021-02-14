3a3ccd50800c15b2e314a8f807aaab2f649742ec
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-02-14 16:14:46 -0300

    Adding Transaction functions

--- src/script.js
+++ src/script.js
@@ -1,12 +1,39 @@
-const modal = {
-    element: document.querySelector('.modal-overlay'),
-    toggle: () => modal.element.classList.toggle('active'),
-}
+const Modal = {
+  element: document.querySelector(".modal-overlay"),
+  toggle: () => modal.element.classList.toggle("active"),
+};
 
-document.querySelector('.button.new').addEventListener('click', () => {
-    modal.toggle()
-})
+document.querySelector(".button.new").addEventListener("click", () => {
+  modal.toggle();
+});
 
-document.querySelector('.button.cancel').addEventListener('click', () => {
-    modal.toggle()
-})
+document.querySelector(".button.cancel").addEventListener("click", () => {
+  modal.toggle();
+});
+
+const trasactions = [
+    {
+        id: 1,
+        description: "Luz",
+        amount: -50000,
+        date: "14/01/2021",
+    },
+    {
+        id: 2,
+        description: "Luz",
+        amount: -50000,
+        date: "14/01/2021",
+    },
+    {
+        id: 3,
+        description: "Luz",
+        amount: -50000,
+        date: "14/01/2021",
+    }
+];
+
+const Trasactions = {
+  incomes: () => {},
+  expenses: () => {},
+  total: (incomes, expenses) => incomes - expenses,
+};
