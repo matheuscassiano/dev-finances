fa84df1886958692562104afe15b48bbd5ff30dd
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-02-14 16:05:46 -0300

    Adding toggle

--- src/script.js
+++ src/script.js
@@ -1,13 +1,12 @@
 const modal = {
     element: document.querySelector('.modal-overlay'),
-    open: () => modal.element.classList.add('active'),
-    close: () => modal.element.classList.remove('active'),
+    toggle: () => modal.element.classList.toggle('active'),
 }
 
 document.querySelector('.button.new').addEventListener('click', () => {
-    modal.open()
+    modal.toggle()
 })
 
 document.querySelector('.button.cancel').addEventListener('click', () => {
-    modal.close()
+    modal.toggle()
 })
